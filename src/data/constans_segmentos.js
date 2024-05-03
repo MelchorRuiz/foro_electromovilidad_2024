7e26d7f41917c885cc4df83963ccf5bc33102bc2
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-05-03 12:34:20 -0600

    segmentos de enfoque

---
+++ src/data/constans_segmentos.js
@@ -0,0 +1,10 @@
+const images = [
+    {src: '/segmentos/transporte.webp', text: 'Transporte eléctrico'},
+    {src: '/segmentos/ensamble.webp', text: 'Nuevas plantas de ensamble de autos eléctricos y la conversión de las plantas de combustión interna'},
+    {src: '/segmentos/infraestructura2.webp', text: 'Infraestructura de carga, generación y almacenamiento'},
+    {src: '/segmentos/baterias.webp', text: 'Baterías'},
+    {src: '/segmentos/politicas.webp', text: 'Políticas gubernamentales y compromisos contraídos internacionalmente'},
+    {src: '/segmentos/semiconductores.webp', text: 'Principales productores de semiconductores, y entorno mundial'},
+]
+
+export {images};
