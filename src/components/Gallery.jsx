d330a282a7f056b4c17d5c9b75d4021526e8953b
Pachi-Dev <pachi.claros@gmail.com>
2024-05-06 10:44:56 -0600

    chore: Update component imports and add menu component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,60 +1,61 @@
-import LightGallery from 'lightgallery/react';
-import React, { useState } from 'react'; // Import the useState hook
+import LightGallery from 'lightgallery/react'
+import React, { useState } from 'react' // Import the useState hook
 
 // import styles
-import 'lightgallery/css/lightgallery.css';
-import 'lightgallery/css/lg-zoom.css';
-import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lightgallery.css'
+import 'lightgallery/css/lg-zoom.css'
+import 'lightgallery/css/lg-thumbnail.css'
 
 // import plugins if you need
-import lgThumbnail from 'lightgallery/plugins/thumbnail';
-import lgZoom from 'lightgallery/plugins/zoom';
+import lgThumbnail from 'lightgallery/plugins/thumbnail'
+import lgZoom from 'lightgallery/plugins/zoom'
 
 import { images } from '../data/constans_gallery'
 
-const imageGroups = [];
+const imageGroups = []
 for (let i = 0; i < images.length; i += 12) {
-    imageGroups.push(images.slice(i, i + 12));
+  imageGroups.push(images.slice(i, i + 12))
 }
 
 export function Gallery() {
-    const onInit = () => {
-        console.log('lightGallery has been initialized');
-    };
+  const onInit = () => {
+    console.log('lightGallery has been initialized')
+  }
 
-    const onAfterOpen = () => {
-        console.log('lightGallery has been opened');
-    }
+  const onAfterOpen = () => {
+    console.log('lightGallery has been opened')
+  }
 
+  const [page, setPage] = useState(0) // Use the useState hook to initialize the page state variable
 
-    const [page, setPage] = useState(0); // Use the useState hook to initialize the page state variable
-
-    return (
-        <>
-            <LightGallery
-                elementClassNames="flex flex-wrap justify-center gap-5"
-                onInit={onInit}
-                onAfterOpen={onAfterOpen}
-                speed={500}
-                plugins={[lgThumbnail, lgZoom]}
-            >
-                {imageGroups[page].map((image, index) => (
-                    <a href={image.src} className='w-[18rem]' key={index}>
-                        <img alt={image.alt} src={image.src} loading="lazy" />
-                    </a>
-                ))}
-            </LightGallery>
-            <div className="flex justify-end gap-2 pt-5">
-                {imageGroups.map((_, index) => (
-                    <button
-                        key={index}
-                        onClick={() => setPage(index)}
-                        className={`px-3 py-1 rounded-md ${page === index ? 'bg-gray-300' : 'bg-gray-200'}`}
-                    >
-                        {index + 1}
-                    </button>
-                ))}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <LightGallery
+        elementClassNames='flex flex-wrap justify-center gap-5'
+        onInit={onInit}
+        onAfterOpen={onAfterOpen}
+        speed={500}
+        plugins={[lgThumbnail, lgZoom]}
+      >
+        {imageGroups[page].map((image, index) => (
+          <a href={image.src} className='w-[18rem]' key={index}>
+            <img alt={image.alt} src={image.src} loading='lazy' />
+          </a>
+        ))}
+      </LightGallery>
+      <div className='flex justify-end gap-2 pt-5'>
+        {imageGroups.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setPage(index)}
+            className={`px-3 py-1 rounded-md ${
+              page === index ? 'bg-gray-300' : 'bg-gray-200'
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </>
+  )
 }
