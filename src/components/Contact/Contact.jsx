b5b0a82e7a8722ad093ebcf6ac18d84eb6afd54d
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-05-09 12:20:29 -0600

    chore: Update Contact component styling and form layout

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,87 @@
+import { useContactFormStore } from '../../store/contact-form'
+import { useState, useEffect } from 'react'
 import './Contact.css'
 
 export function Contact() {
+  const { name, email, phone, company, position, state, city, setName, setEmail, setPhone, setCompany, setPosition, setState, setCity } = useContactFormStore()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(`Gracias por enviar tus datos, ${name}!`)
+  }
+
+  const [states, setStates] = useState([])
+  const [cities, setCities] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/CL_GEO_AREA/null/es/BISE/2.0/e6593d42-e989-a9ee-117d-f4bf12620431?type=json');
+      const { CODE } = await response.json();
+      const states = CODE.filter((code) => code.value.length === 8).slice(0, 32);
+      setStates(states);
+    };
+  
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/CL_GEO_AREA/null/es/BISE/2.0/e6593d42-e989-a9ee-117d-f4bf12620431?type=json');
+      const { CODE } = await response.json();
+      const currentState = CODE.find((code) => code.Description === state);
+      const cities = currentState ? CODE.filter((code) => code.value.startsWith(currentState.value)).slice(0, -4) : [];
+      setCities(cities);
+    };
+  
+    fetchData();
+  }, [state])
+
   return (
-    <>
-    </>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nombre</label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="phone">Teléfono</label>
+        <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="company">Empresa</label>
+        <input type="text" value={company} onChange={(e) => setCompany(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="position">Cargo</label>
+        <input type="text" value={position} onChange={(e) => setPosition(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="state">Estado</label>
+        <select value={state} onChange={(e) => setState(e.target.value)}>
+          <option value="" disabled >Selecciona un estado</option>
+          {states.map((state) => (
+            <option key={state.value} value={state.Description}>
+              {state.Description}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="city">Municipio</label>
+        <select value={city} onChange={(e) => setCity(e.target.value)}>
+          <option value="" disabled >Selecciona un municipio</option>
+          {cities.map((city) => (
+            <option key={city.value} value={city.Description}>
+              {city.Description}
+            </option>
+          ))}
+        </select>
+      </div>
+      <br />
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
