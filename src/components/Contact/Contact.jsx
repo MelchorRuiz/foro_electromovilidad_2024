a0119055a70743219b95b1666de98806f4305dc4
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-05-13 16:41:49 -0600

    refactor: Update Contact component form handling with react-hook-form and optimize form layout

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,29 @@
-import { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
-
 import { useContactFormStore } from '../../store/contact-form'
 import { locationData } from '../../data/constans_states_and_cities'
-import useFormEffects from './useFormEffects';
 import { InputField, SelectField } from './Fields.jsx';
 import './Contact.css'
 
 export function Contact() {
-  const { name, email, phone, company, position, state, city } = useContactFormStore();
+  const { 
+    name, 
+    email, 
+    phone, 
+    company, 
+    position, 
+    state, 
+    city, 
+    setName,
+    setEmail,
+    setPhone,
+    setCompany,
+    setPosition,
+    setState, 
+    setCity } = useContactFormStore();
 
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors }
   } = useForm();
 
@@ -21,47 +31,39 @@ export function Contact() {
     console.log(data)
   }
 
-  const [states, setStates] = useState([])
-  const [cities, setCities] = useState([])
-  const name_watch = watch('name')
-  const email_watch = watch('email')
-  const phone_watch = watch('phone')
-  const company_watch = watch('company')
-  const position_watch = watch('position')
-  const state_watch = watch('state')
-  const city_watch = watch('city')
-
-  useEffect(() => {
-    const states = Object.keys(locationData)
-    setStates(states)
-  }, [])
-
-  useEffect(() => {
-    if (state) {
-      const cities = locationData[state]
-      setCities(cities)
-    }
-  }, [state])
-
-  const watchValues = { name_watch, email_watch, phone_watch, company_watch, position_watch, state_watch, city_watch }
-  useFormEffects(watchValues);
+  const states = Object.keys(locationData)
+  const cities = state ? locationData[state] : []
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+
       <InputField label="Nombre" defaultValue={name} register={register} errors={errors} name="name"
-        validation={{ required: 'El nombre es requerido' }} />
+        validation={{ required: 'El nombre es requerido' }} handleValue={setName}/>
+
       <InputField label="Email" defaultValue={email} register={register} errors={errors} name="email"
-        validation={{ required: 'El email es requerido', pattern: { value: /^\S+@\S+$/i, message: 'Email inválido' } }} />
+        validation={{ required: 'El email es requerido', pattern: { value: /^\S+@\S+$/i, message: 'Email inválido' } }} 
+        handleValue={setEmail}/>
+
       <InputField label="Teléfono" defaultValue={phone} register={register} errors={errors} name="phone"
-        validation={{ required: 'El teléfono es requerido', pattern: { value: /^\d+$/, message: 'Teléfono inválido' } }} />
+        validation={{ required: 'El teléfono es requerido', pattern: { value: /^\d+$/, message: 'Teléfono inválido' } }} 
+        handleValue={setPhone}/>
+
       <InputField label="Empresa" defaultValue={company} register={register} errors={errors} name="company"
-        validation={{ required: 'La empresa es requerida' }} />
+        validation={{ required: 'La empresa es requerida' }} 
+        handleValue={setCompany}/>
+
       <InputField label="Cargo" defaultValue={position} register={register} errors={errors} name="position"
-        validation={{ required: 'El cargo es requerido' }} />
+        validation={{ required: 'El cargo es requerido' }} 
+        handleValue={setPosition}/>
+
       <SelectField label="Estado" options={states} defaultValue={state} register={register} errors={errors} name="state"
-        validation={{ required: 'El estado es requerido' }} />
+        validation={{ required: 'El estado es requerido' }} 
+        handleValue={setState}/>
+
       <SelectField label="Municipio" options={cities} defaultValue={city} register={register} errors={errors} name="city"
-        validation={{ required: 'El municipio es requerido' }} />
+        validation={{ required: 'El municipio es requerido' }} 
+        handleValue={setCity}/>
+
       <br />
       <button type="submit">Enviar</button>
     </form>
--- src/components/Contact/Fields.jsx
+++ src/components/Contact/Fields.jsx
@@ -1,18 +1,24 @@
-export const InputField = ({ label, defaultValue, register, errors, name, validation }) => (
+export const InputField = ({ label, defaultValue, register, errors, name, validation, handleValue }) => (
   <div>
     <label>{label}</label>
-    <input defaultValue={defaultValue} {...register(name, validation)} />
+    <input defaultValue={defaultValue} {...register(name, {
+      ...validation,
+      onChange: (e) => handleValue(e.target.value)
+    })} />
     {errors[name] && <span>{errors[name].message}</span>}
   </div>
 );
 
-export const SelectField = ({ label, options, defaultValue, register, errors, name, validation }) => (
+export const SelectField = ({ label, options, defaultValue, register, errors, name, validation, handleValue }) => (
   <div>
-    <label>{label}</label>
-    <select value={defaultValue} {...register(name, validation)}>
+    <label htmlFor={name}>{label}</label>
+    <select value={defaultValue} {...register(name, {
+      ...validation,
+      onChange: (e) => handleValue(e.target.value)
+    })}>
       <option value="">Selecciona una opción</option>
-      {options.map((option) => (
-        <option key={option} value={option}>
+      {options.map((option, index) => (
+        <option key={index} value={option} >
           {option}
         </option>
       ))}
