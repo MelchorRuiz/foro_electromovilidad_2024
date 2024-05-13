08b29b4e4764ae2a67eb113674b38d3d0a1eb228
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-05-13 09:24:11 -0600

    refactor: Rename location_data to locationData in constants_states_and_cities.js

--- src/data/constans_states_and_cities.js
+++ src/data/constans_states_and_cities.js
@@ -1,4 +1,4 @@
-export const location_data ={
+export const locationData ={
   Aguascalientes: ['Aguascalientes', 'Asientos', 'Calvillo', 'Cosio', 'El Llano', 'Jesus Maria', 'Pabellon de Arteaga', 'Rincon de Romos', 'San Francisco de los Romo', 'San Jose de Gracia', 'Tepezala'],
   'Baja California': ['Ensenada', 'Mexicali', 'Playas de Rosarito', 'Tecate', 'Tijuana'],
   'Baja California Sur': ['Comondu', 'La Paz', 'Loreto', 'Los Cabos', 'Mulege'],
