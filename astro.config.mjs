01402d23a6f5a536004da93238c4314850c3fc31
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-06-24 12:18:01 -0600

    change i18n integration to native astro internationalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astroI18next()]
+  integrations: [tailwind(), react()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    fallback: {
+      "en": "es"
+    }
+  }
 });
--- src/data/constans_segmentos.js
+++ src/data/constans_segmentos.js
@@ -1,10 +1,10 @@
 const images = [
-    {src: '/segmentos/transporte.webp', text: 'focus-segments.segments.1'},
-    {src: '/segmentos/ensamble.webp', text: 'focus-segments.segments.2'},
-    {src: '/segmentos/infraestructura2.webp', text: 'focus-segments.segments.3'},
-    {src: '/segmentos/baterias.webp', text: 'focus-segments.segments.4'},
-    {src: '/segmentos/politicas.webp', text: 'focus-segments.segments.5'},
-    {src: '/segmentos/semiconductores.webp', text: 'focus-segments.segments.6'},
+    {src: '/segmentos/transporte.webp'},
+    {src: '/segmentos/ensamble.webp'},
+    {src: '/segmentos/infraestructura2.webp'},
+    {src: '/segmentos/baterias.webp'},
+    {src: '/segmentos/politicas.webp'},
+    {src: '/segmentos/semiconductores.webp'},
 ]
 
 export {images};
