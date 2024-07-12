6f95806ad6908453e02105c63bfa19559d272cb7
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-07-12 17:16:49 -0600

    refactor: Add SQL script for creating users table

--- sql/database.sql
+++ sql/database.sql
@@ -18,4 +18,6 @@ CREATE TABLE users (
   state varchar(50) NOT NULL,
   city varchar(50) NOT NULL,
   created_at timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
+
+SELECT * FROM users;
